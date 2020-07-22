421eb6d7ba7916098fa0cbfb228383aa99063b96
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-22 18:39:20 -0500

    ADD validates options for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true, length: { within: 4..12 }
+  validates :password, presence: true, length: { within 4..12 }
+  validates :email, presence: true, uniqueness: true
 end
