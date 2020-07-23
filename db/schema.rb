71914e2d5de5ba7e242ecf91409aaa83a8aadfc1
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-22 20:20:56 -0500

    CREATE Assosiation between User and Post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_232126) do
+ActiveRecord::Schema.define(version: 2020_07_23_011906) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
@@ -18,6 +26,10 @@ ActiveRecord::Schema.define(version: 2020_07_22_232126) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.integer "post_id"
+    t.index ["post_id"], name: "index_users_on_post_id"
+    t.index ["user_id"], name: "index_users_on_user_id"
   end
 
 end
