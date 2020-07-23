37cb3894089660dc2044feeb320ac1293d5a15b1
emiliocm9 <emilio.contreras97@gmail.com>
2020-07-22 23:14:01 -0500

    CREATE comments validations and test

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
 
-  validations :user_id, :post_id, presence: true
-  validations :author, presence: true, length: { within: 4..20 }
-  validations :body, presence: true, length: { maximum: 100 }
+  validates :user_id, :post_id, presence: true
+  validates :author, presence: true, length: { within: 4..20 }
+  validates :body, presence: true, length: { maximum: 100 }
 end
